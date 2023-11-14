37faa227fa89a416cfb5c034952604182eb0d272
ak4m410x01 <abdallah10kamal@gmail.com>
2023-11-14 11:10:20 +0200

    596. Classes More Than 5 Students

---
+++ 596. Classes More Than 5 Students.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    class
+FROM
+    (
+        SELECT
+            class,
+            COUNT(student) AS students
+        FROM
+            courses
+        GROUP BY
+            class
+    ) AS class_students
+WHERE
+    students >= 5;
