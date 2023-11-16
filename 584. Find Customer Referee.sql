393f5ac17287fa5fa8778b67bf499f1043f920a3
ak4m410x01 <abdallah10kamal@gmail.com>
2023-11-16 06:39:15 +0200

    584. Find Customer Referee

--- 584. Find Customer Referee.sql
+++ 584. Find Customer Referee.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT
     name
 FROM
-    `Customer`
+    Customer
 WHERE
     referee_id != 2
     OR referee_id IS NULL;
