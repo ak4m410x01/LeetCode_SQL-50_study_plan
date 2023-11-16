236b5681aec423e2b103c789d9d3d5c011bc2506
ak4m410x01 <abdallah10kamal@gmail.com>
2023-11-16 17:45:11 +0200

    1661. Average Time of Process per Machine

--- 1661. Average Time of Process per Machine.sql
+++ 1661. Average Time of Process per Machine.sql
@@ -1,12 +1,13 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT
     st.machine_id,
-    ROUND(AVG(ed.timestamp - st.timestamp), 3) AS processing_time
+    ROUND(
+        CAST(AVG(ed.timestamp - st.timestamp) AS numeric),
+        3
+    ) AS processing_time
 FROM
-    `Activity` AS st
-    JOIN `Activity` AS ed
-WHERE
-    st.machine_id = ed.machine_id
+    activity AS st
+    JOIN activity AS ed ON st.machine_id = ed.machine_id
     AND st.process_id = ed.process_id
     AND st.activity_type = 'start'
     AND ed.activity_type = 'end'
