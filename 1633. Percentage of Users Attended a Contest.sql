af94eb2302fa98ac86564a1c6d608d0db9a8ab48
ak4m410x01 <abdallah10kamal@gmail.com>
2023-11-16 18:48:37 +0200

    1633. Percentage of Users Attended a Contest

--- 1633. Percentage of Users Attended a Contest.sql
+++ 1633. Percentage of Users Attended a Contest.sql
@@ -1,17 +1,19 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT
     contest_id,
     ROUND(
-        COUNT(Register.user_id) * 100 /(
-            SELECT
-                COUNT(user_id)
-            FROM
-                `Users`
-        ),
+        (
+            COUNT(Register.user_id) * 100 /(
+                SELECT
+                    COUNT(user_id)
+                FROM
+                    Users
+            ) :: FLOAT
+        ) :: NUMERIC,
         2
     ) AS percentage
 FROM
-    `Register`
+    Register
 GROUP BY
     contest_id
 ORDER BY
