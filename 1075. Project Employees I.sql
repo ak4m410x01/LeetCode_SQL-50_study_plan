b1f330b72174ce4e4d818fa319fa40cce073945a
ak4m410x01 <abdallah10kamal@gmail.com>
2023-11-16 18:46:14 +0200

    1075. Project Employees I

--- 1075. Project Employees I.sql
+++ 1075. Project Employees I.sql
@@ -1,9 +1,9 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT
     Project.project_id,
     ROUND(AVG(Employee.experience_years), 2) AS average_years
 FROM
-    `Employee`
-    JOIN `Project` ON Employee.employee_id = Project.employee_id
+    Employee
+    JOIN Project ON Employee.employee_id = Project.employee_id
 GROUP BY
     Project.project_id;
