2327bd65225b90803b5b0f4c6bb9265175fc35c0
ak4m410x01 <abdallah10kamal@gmail.com>
2023-11-16 07:25:39 +0200

    1378. Replace Employee ID With The Unique Identifier

--- 1378. Replace Employee ID With The Unique Identifier.sql
+++ 1378. Replace Employee ID With The Unique Identifier.sql
@@ -1,7 +1,7 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT
     EmployeeUNI.unique_id,
     Employees.name
 FROM
-    `Employees`
-    LEFT JOIN `EmployeeUNI` ON Employees.id = EmployeeUNI.id;
+    Employees
+    LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id;
