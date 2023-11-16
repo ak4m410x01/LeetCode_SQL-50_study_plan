d39403db66ae49cad5043ec187436af34c828cff
ak4m410x01 <abdallah10kamal@gmail.com>
2023-11-16 07:28:20 +0200

    1581. Customer Who Visited but Did Not Make Any Transactions

--- 1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ 1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,15 +1,15 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT
     customer_id,
     COUNT(visit_id) AS count_no_trans
 FROM
-    `Visits`
+    Visits
 WHERE
     visit_id NOT IN (
         SELECT
             DISTINCT visit_id
         FROM
-            `Transactions`
+            Transactions
     )
 GROUP BY
     customer_id
